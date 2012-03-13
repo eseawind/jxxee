d93e60e08bf908cdb7b9b4ed8a017191daf7a87d
myjxstar <myjxstar@gmail.com>
2012-03-13 07:34:02 +0000

    调整SQL字段的长度

--- jxstar-webapp/src/main/webapp/jxstar/studio/form_rule_route.js
+++ jxstar-webapp/src/main/webapp/jxstar/studio/form_rule_route.js
@@ -14,23 +14,21 @@
 			autoHeight:true,
 			items:[{
 				border:false,
-				columnWidth:0.33,
+				columnWidth:0.495,
 				layout:'form',
 				style: 'padding-left:10px;',
 				items:[
 					{xtype:'textfield', fieldLabel:'来源功能ID', name:'fun_rule_route__src_funid', allowBlank:false, labelStyle:'color:#0000FF;', labelSeparator:'*', anchor:'100%', maxLength:25},
 					{xtype:'textfield', fieldLabel:'目标功能ID', name:'fun_rule_route__fun_id', readOnly:true, anchor:'100%', maxLength:25},
-					{xtype:'hidden', fieldLabel:'参数类型', name:'fun_rule_route__where_type', anchor:'100%'},
-					{xtype:'hidden', fieldLabel:'页面参数名', name:'fun_rule_route__where_value', anchor:'100%'},
-					{xtype:'hidden', fieldLabel:'路由ID', name:'fun_rule_route__route_id', anchor:'100%'}
+					{xtype:'hidden', fieldLabel:'路由ID', name:'fun_rule_route__route_id', anchor:'62%'},
+					{xtype:'hidden', fieldLabel:'参数类型', name:'fun_rule_route__where_type', anchor:'62%'}
 				]
 			},{
 				border:false,
-				columnWidth:0.66,
+				columnWidth:0.495,
 				layout:'form',
 				style: 'padding-left:10px;',
 				items:[
-					{xtype:'textarea', fieldLabel:'来源功能Where', name:'fun_rule_route__where_sql', width:'100%', height:48, maxLength:200},
 					{xtype:'trigger', fieldLabel:'导入布局', name:'fun_rule_route__layout_page',
 						anchor:'100%', triggerClass:'x-form-search-trigger',
 						maxLength:100, editable:true,
@@ -41,7 +39,23 @@
 								JxSelect.createComboGrid(selcfg, this.menu, 'node_rule_route_form');
 							}
 							this.menu.show(this.el);
-						}}
+						}},
+					{xtype:'hidden', fieldLabel:'页面参数名', name:'fun_rule_route__where_value', anchor:'62%'}
+				]
+			}
+			]
+		},{
+			anchor:'100%',
+			border: false,
+			layout:'column',
+			autoHeight:true,
+			items:[{
+				border:false,
+				columnWidth:0.99,
+				layout:'form',
+				style: 'padding-left:10px;',
+				items:[
+					{xtype:'textarea', fieldLabel:'来源功能Where', name:'fun_rule_route__where_sql', width:'100%', height:72, maxLength:500}
 				]
 			}
 			]
