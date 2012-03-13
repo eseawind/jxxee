b022422816fec51d8e5cbf0186c545a10dedafeb
myjxstar <myjxstar@gmail.com>
2012-03-13 12:39:13 +0000

    添加已办任务查看功能

--- jxstar-webapp/src/main/webapp/jxstar/studio/grid_wfassign_his.js
+++ jxstar-webapp/src/main/webapp/jxstar/studio/grid_wfassign_his.js
@@ -2,10 +2,10 @@
 	var config = {param:{},initpage:function(page, define){},eventcfg:{}};
 
 	var cols = [
-	{col:{header:'任务名称', width:260, sortable:true}, field:{name:'wf_instance__process_name',type:'string'}},
+	{col:{header:'任务名称', width:260, sortable:true}, field:{name:'wf_process__process_name',type:'string'}},
 	{col:{header:'任务数量', width:120, sortable:true, renderer:JxUtil.formatInt()}, field:{name:'process_num',type:'int'}},
-	{col:{header:'过程ID', width:100, sortable:true, hidden:true}, field:{name:'wf_instance__process_id',type:'string'}},
-	{col:{header:'功能ID', width:100, sortable:true, hidden:true}, field:{name:'wf_assign__fun_id',type:'string'}}
+	{col:{header:'过程ID', width:100, sortable:true, hidden:true}, field:{name:'wf_process__process_id',type:'string'}},
+	{col:{header:'功能ID', width:100, sortable:true, hidden:true}, field:{name:'wf_assignhis__fun_id',type:'string'}}
 	];
 	
 	config.param = {
@@ -14,16 +14,16 @@
 		hasQuery: '0',
 		isedit: '0',
 		isshow: '0',
-		funid: 'wf_assign_num'
+		funid: 'wf_assignhis'
 	};
 	
 	//把任务数量字段值改为超链接
 	var renderTask = function(val, metaData, record) {
-		var funId = record.get('wf_assign__fun_id');
+		var funId = record.get('wf_assignhis__fun_id');
 		var userId = JxDefault.getUserId();
 		
 		var chgcolor = 'onmouseover="this.style.color=\'#FF4400\';" onmouseout="this.style.color=\'#0080FF\';"';
-		var html = '<a href="#" style=\'color:#0080FF;\' '+ chgcolor +' onclick="JxUtil.showCheckData(\''+ funId +'\', \'\', \''+ userId +'\');">&nbsp;'+ val +'&nbsp;</a>';
+		var html = '<a href="#" style=\'color:#0080FF;\' '+ chgcolor +' onclick="JxUtil.showCheckHisData(\''+ funId +'\', \'\', \''+ userId +'\');">&nbsp;'+ val +'&nbsp;</a>';
 		return html;
 	};
 	
@@ -37,7 +37,7 @@
 	config.initpage = function(gridNode){
 		var grid = gridNode.page;
 		
-		var wsql = 'wf_assign.assign_userid = ?';
+		var wsql = 'wf_assignhis.check_userid = ?';
 		var wvalue = JxDefault.getUserId();
 		var wtype = 'string';
 		Jxstar.loadData(grid, {where_sql:wsql, where_value:wvalue, where_type:wtype, has_page:'0'});
