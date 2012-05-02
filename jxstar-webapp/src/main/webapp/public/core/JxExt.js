f22b0cffc59120af08e9f5dd0a9b4a225c6049e6
myjxstar <myjxstar@gmail.com>
2012-05-02 01:14:08 +0000

    修改urlEncode方法

--- jxstar-webapp/src/main/webapp/public/core/JxExt.js
+++ jxstar-webapp/src/main/webapp/public/core/JxExt.js
@@ -11,7 +11,7 @@
  */
  
 /**
- * 在数组中插入对象
+ * 新增方法：在数组中插入对象
  * index -- 指要插入的位置，从0开始，如果是负数，则从尾部开始
  * item -- 指要插入的项目
  **/
@@ -32,41 +32,41 @@ Array.prototype.insert = function(index, item) {
 };
 
 /**
- * 定期清理孤立节点
+ * 修改属性：定期清理孤立节点
  **/
 Ext.enableListenerCollection = true;
 
 /**
- * 扩展Ext组件的功能。
- * 把Date对象格式化yyyy-mm-dd格式的字符串。
+ * ext-3.3.1
+ * 修改方法：把Date对象格式化yyyy-mm-dd格式的字符串。
  **/
 Ext.urlEncode = function(o, pre){
-	var undef, buf = [], key, e = encodeURIComponent;
-
-	for(key in o){
-		undef = !Ext.isDefined(o[key]);
-		Ext.each(undef ? key : o[key], function(val, i){
-			val = Ext.isDate(val) ? val.dateFormat('Y-m-d H:i:s') : val;//by bingco add line. format date object to string.
-			buf.push("&", e(key), "=", (val != key || !undef) ? e(val) : "");
+	var empty, buf = [], e = encodeURIComponent;
+	
+	Ext.iterate(o, function(key, item){
+		empty = Ext.isEmpty(item);
+		Ext.each(empty ? key : item, function(val){
+			//Ext.encode(val).replace(/"/g, '') --> val.dateFormat('Y-m-d H:i:s') //modify by tony
+			buf.push('&', e(key), '=', (!Ext.isEmpty(val) && (val != key || !empty)) ? (Ext.isDate(val) ? val.dateFormat('Y-m-d H:i:s') : e(val)) : '');
 		});
-	}
+	});
 	if(!pre){
 		buf.shift();
-		pre = "";
+		pre = '';
 	}
 	return pre + buf.join('');
 };
 
 /**
- * 扩展Ext组件的功能。
- * 分页栏刷新数据时加上上次的参数。
+ * ext-3.3.1
+ * 修改方法：分页栏刷新数据时加上上次的参数。
  **/
 Ext.PagingToolbar.prototype.doLoad = function(start){
-	var options = this.store.lastOptions;//by bingco add line
-    var o = {};
-	if (options != null && options.params != null) {
+	var o = {};
+	var options = this.store.lastOptions;//---add by tony
+	if (options && options.params) {
 		o = options.params;
-	}
+	}//---add by
 	
 	var pn = this.getParams();
 	o[pn.start] = start;
@@ -77,12 +77,13 @@ Ext.PagingToolbar.prototype.doLoad = function(start){
 };
 
 /**
- * 支持设置表格头部的样式
+ * ext-3.3.1
+ * 修改方法：支持设置表格头部的样式
  **/ 
 Ext.grid.GridView.prototype.getColumnStyle = function(colIndex, isHeader){
 	var colModel  = this.cm,
 		colConfig = colModel.config,
-		style     = isHeader ? colConfig[colIndex].hcss || '' : colConfig[colIndex].css || '',//给cm添加了hcss属性
+		style     = isHeader ? colConfig[colIndex].hcss || '' : colConfig[colIndex].css || '',//添加了属性：colConfig[colIndex].hcss || 
 		align     = colConfig[colIndex].align;
 	
 	style += String.format("width: {0};", this.getColumnWidth(colIndex));
@@ -98,7 +99,13 @@ Ext.grid.GridView.prototype.getColumnStyle = function(colIndex, isHeader){
 	return style;
 };
 
-//unselectable="on" add x-selectable
+/**
+ * ext-3.3.1
+ * 修改方法：修改表格单元可选择字符，1行增加：x-selectable，二行删除：unselectable="on"，原：
+ * '<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} {css}" style="{style}" tabIndex="0" {cellAttr}>',
+ *     '<div class="x-grid3-cell-inner x-grid3-col-{id}" unselectable="on" {attr}>{value}</div>',
+ * '</td>'
+ **/ 
 Ext.grid.GridView.prototype.cellTpl = new Ext.Template(
 	'<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>',
 		'<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>',
@@ -106,7 +113,8 @@ Ext.grid.GridView.prototype.cellTpl = new Ext.Template(
 );
 
 /**
- * 去掉属性表格中按属性名排序的特性
+ * ext-3.3.1
+ * 修改方法：去掉属性表格中按属性名排序的特性
  **/
 Ext.grid.PropertyGrid.prototype.initComponent = function(){
 	this.customRenderers = this.customRenderers || {};
@@ -115,7 +123,7 @@ Ext.grid.PropertyGrid.prototype.initComponent = function(){
 	var store = new Ext.grid.PropertyStore(this);
 	this.propStore = store;
 	var cm = new Ext.grid.PropertyColumnModel(this, store);
-	//store.store.sort('name', 'ASC');	//delete this line.
+	//store.store.sort('name', 'ASC');	//delete by tony
 	this.addEvents(
 		'beforepropertychange',
 		'propertychange'
@@ -133,35 +141,34 @@ Ext.grid.PropertyGrid.prototype.initComponent = function(){
 };
 
 /**
- * 扩展Ext组件的功能。
- * 取checkbox的值时取 0 或 1，而不是true false。
+ * ext-3.3.1
+ * 修改方法：取checkbox的值时取 0 或 1，而不是true false。
  **/
 Ext.form.Checkbox.prototype.getValue = function(){
 	if(this.rendered){
-		return this.el.dom.checked ? '1' : '0';//add line
+		return this.el.dom.checked ? '1' : '0';
 	}
-	return '0';
-};	
+	return this.checked ? '1' : '0';
+};
 
 /**
- * 扩展Ext.tree.TreeLoader组件的功能。
- * 处理返回结果。
+ * ext-3.3.1
+ * 修改方法：把返回结果中的树形数据作为响应数据
  **/
 Ext.tree.TreeLoader.prototype.handleResponse = function(response){
 	this.transId = false;
 	var a = response.argument;
-	var d = Ext.decode(response.responseText).data;//by bingco add line-----
+	var d = Ext.decode(response.responseText).data;//---add by tony
 	if (d != null) {
 		response.responseText = Ext.encode(d);
-	}												//-----------
+	}//----add by
 	
 	this.processResponse(response, a.node, a.callback, a.scope);
 	this.fireEvent("load", this, a.node, response);
 };
 
 /**
- * 扩展Ext.form.BasicForm组件的功能。
- * 模拟record的set/get方法，在JxSelect.setSelectData中使用。
+ * 新增方法：模拟record的set/get方法，在JxSelect.setSelectData中使用。
  **/
 Ext.form.BasicForm.prototype.set = function(name, value) {
 	var f = this.findField(name);
@@ -175,8 +182,7 @@ Ext.form.BasicForm.prototype.set = function(name, value) {
 };
 
 /**
- * 扩展Ext.form.BasicForm组件的功能。
- * 模拟record的set/get方法，在JxSelect.setSelectData中使用。
+ * 新增方法：模拟record的set/get方法，在JxSelect.setSelectData中使用。
  **/
 Ext.form.BasicForm.prototype.get = function(name) {
 	var f = this.findField(name);
@@ -185,6 +191,10 @@ Ext.form.BasicForm.prototype.get = function(name) {
 	}
 	return '';
 };
+
+/**
+ * 新增方法：取数值。
+ **/
 Ext.form.BasicForm.prototype.getNum = function(name) {
 	var value = '';
 	var f = this.findField(name);
@@ -197,8 +207,7 @@ Ext.form.BasicForm.prototype.getNum = function(name) {
 };
 
 /**
- * 扩展Ext.form.BasicForm组件的功能。
- * 保证修改字段值后不标记为脏数据。
+ * 新增方法：保证修改字段值后不标记为脏数据。
  **/
 Ext.form.BasicForm.prototype.oset = function(name, value) {
 	var f = this.findField(name);
@@ -212,7 +221,7 @@ Ext.form.BasicForm.prototype.oset = function(name, value) {
 };
 
 /**
-* 取当前表单的所有字段，含组合字段内的字段
+* 新增方法：取当前表单的所有字段，含组合字段内的字段
 **/
 Ext.form.BasicForm.prototype.fieldItems = function() {
 	var fields = new Ext.util.MixedCollection(false, function(o){
@@ -232,8 +241,7 @@ Ext.form.BasicForm.prototype.fieldItems = function() {
 };
 
 /**
- * 扩展Ext.form.Field组件的功能。
- * 保证修改字段值后不标记为脏数据。
+ * 新增方法：保证修改字段值后不标记为脏数据。
  **/
 Ext.form.Field.prototype.osetValue = function(value){
 	this.setValue(value);
@@ -241,32 +249,31 @@ Ext.form.Field.prototype.osetValue = function(value){
 };
 
 /**
- * 扩展Ext.form.DateField组件的功能。
- * 支持在选择日期的时候，自动带上时间值，设置样式：Y-m-d H:i，就支持显示时间。
+ * ext-3.3.1
+ * 修改方法：支持在选择日期的时候，自动带上时间值，设置样式：Y-m-d H:i，就支持显示时间。
  **/
 Ext.form.DateField.prototype.onSelect = function(m, d){
-	if (Ext.isDate(d)) {
+	if (Ext.isDate(d)) {//---add by tony
 		var curd = new Date();
 		d.setHours(curd.getHours(), curd.getMinutes(), curd.getSeconds());
-	}
-	//------------------------------up by bingco add -----------------------
+	}//---add by
 	this.setValue(d);
 	this.fireEvent('select', this, d);
 	this.menu.hide();
 };
 
 /**
- * 扩展Ext.form.DateField组件的功能。
- * 如果是样式：Y-m，在显示日期值取的是当前日，应该取1号
+ * ext-3.3.1
+ * 修改方法：如果是样式Y-m，在显示日期值取的是当前日，应该取1号
  **/
 Ext.form.DateField.prototype.parseDate = function(value) {
 	if(!value || Ext.isDate(value)){
 		return value;
 	}
-	//------------------------------add by tony.tan -----------------------
-	if (!Ext.isEmpty(value) && value.length == 7 && this.format == 'Y-m') {
+	
+	if (!Ext.isEmpty(value) && value.length == 7 && this.format == 'Y-m') {//---add by tony
 		return Date.parseDate(value+'-01', 'Y-m-d');
-	}
+	}//---add by
 
 	var v = this.safeParse(value, this.format),
 		af = this.altFormats,
@@ -283,87 +290,85 @@ Ext.form.DateField.prototype.parseDate = function(value) {
 },
 
 /**
- * 修改FormLayout的标签描述不添加':'符号。
+ * 修改属性：FormLayout的标签描述不添加':'符号。
  **/
 Ext.layout.FormLayout.prototype.labelSeparator = '';
 
 /**
- * 修改NumberField缺省不允许输入负数。
+ * 修改属性：NumberField缺省不允许输入负数。
  **/
 //Ext.form.NumberField.prototype.allowNegative = false;
 
 /**
- * 修改NumberField聚焦则全选。
+ * 修改属性：NumberField聚焦则全选。
  **/
 //Ext.form.NumberField.prototype.selectOnFocus = true;
 
 /**
- * 修改TextField聚焦则全选。
+ * 修改属性：TextField聚焦则全选。
  **/
 //Ext.form.TextField.prototype.selectOnFocus = true;
 
 /**
- * 修改BasicForm加载数据后设置为初始值。
+ * 修改属性：BasicForm加载数据后设置为初始值。
  **/
 Ext.form.BasicForm.prototype.trackResetOnLoad = true;
 
 /**
- * 修改Component状态支持缺省值为否，设置所有控件都不保存状态。
+ * 修改属性：Component状态支持缺省值为否，设置所有控件都不保存状态。
  **/
 Ext.Component.prototype.stateful = false;
 
 /**
- * 修改对话框的缺省标题。
+ * 修改属性：对话框的缺省标题。
  **/
 Ext.Window.prototype.iconCls = 'eb_win';
 
 /**
- * 修改对话框的缺省不带阴影。
+ * 修改属性：对话框的缺省不带阴影。
  **/
 Ext.Window.prototype.shadow = false;
 
 /**
- * 处理重复打开combogrid页面时报下面的错误。
+ * ext-3.3.1
+ * 修改方法：处理重复打开combogrid页面时报下面的错误。
  **/
 Ext.layout.MenuLayout.prototype.isValidParent = function(c, target) {
 	var el = c.el.up('li.x-menu-list-item', 5);
-	if (el == null) return false;
+	if (Ext.isEmpty(el)) return false;//add by tony
 	return el.dom.parentNode === (target.dom || target);
 };
 
 /**
- * 修改字段控件，如果是只读，则需要添加只读样式。
+ * ext-3.3.1
+ * 修改方法：如果是只读，则需要添加只读样式。
  **/
 Ext.form.Field.prototype.setReadOnly = function(readOnly){
 	if(this.rendered){
 		this.el.dom.readOnly = readOnly;
-		//add by tony.tan
-		if (readOnly) {
+		if (readOnly) {//---add by tony
 			this.el.addClass('x-field-only');
 		} else {
 			this.el.removeClass('x-field-only');
-		}
-		//----------------
+		}//---add by
 	}
 	this.readOnly = readOnly;
 };
 Ext.form.TriggerField.prototype.setReadOnly = function(readOnly){
 	if(readOnly != this.readOnly){
-		//add by tony.tan
-		if (readOnly) {
+		if (readOnly) {//---add by tony
 			this.el.addClass('x-field-only');
 		} else {
 			this.el.removeClass('x-field-only');
-		}
-		//----------------
-	
+		}//---add by
 		this.readOnly = readOnly;
 		this.updateEditState();
 	}
 };
 
 /**
- * 修改字段选择控件，如果是只读，则需要添加只读样式。
+ * ext-3.3.1
+ * 修改方法：如果是只读，则需要添加只读样式。
  **/
 Ext.form.TriggerField.prototype.updateEditState = function(){
 	if(this.rendered){
@@ -392,7 +397,10 @@ Ext.form.TriggerField.prototype.updateEditState = function(){
 	}
 };
 
-//修改下面一行代码处理日期控件的按钮只读后还可以选择的问题
+/**
+ * ext-3.3.1
+ * 修改方法：处理日期控件的按钮只读后还可以选择的问题
+ **/
 Ext.form.DateField.prototype.onTriggerClick = function(){
 	if(this.readOnly || this.disabled){//modify by tony.tan add 'this.readOnly || '
 		return;
@@ -433,7 +441,8 @@ Ext.form.DateField.prototype.fieldClass = 'x-field-d';
 Ext.form.NumberField.prototype.fieldClass = 'x-field-n';
 
 /**
- * 添加F1 -- F12为特殊键，用于处理字段的帮助信息CTRL+F1。
+ * ext-3.3.1
+ * 修改方法：添加F1 -- F12为特殊键，用于处理字段的帮助信息CTRL+F1。
  **/
 Ext.EventObjectImpl.prototype.isSpecialKey = function(){
    var k = this.normalizeKey(this.keyCode);
@@ -446,11 +455,12 @@ Ext.EventObjectImpl.prototype.isSpecialKey = function(){
 };
 	
 /**
- * 用来解决RowEditor类中这行ed = c.getEditor(); is not a function的错误
+ * 新增方法：用来解决RowEditor类中这行ed = c.getEditor(); is not a function的错误
  **/
 Ext.grid.RowSelectionModel.prototype.getEditor = Ext.emptyFn;
 
 /**
+ * ext-3.3.1
  * 问题：如果int，float类型的值为null时，在record中取到后为0；
  * 分析：在Ext.data.JsonReader.extractValues中转换值时改变了，分析是Types.INT FLOAT的两个方法转换了，增加了this.useNull判断
  *       实际上是判断field对象的属性useNull，所以增加下面的一行，保留数值可以显示null
@@ -460,7 +470,7 @@ Ext.grid.RowSelectionModel.prototype.getEditor = Ext.emptyFn;
 Ext.data.Types.INT = {
 	convert: function(v){
 		return v !== undefined && v !== null && v !== '' ?
-			parseInt(String(v).replace(Ext.data.Types.stripRe, ''), 10) : '';//(this.useNull ? null : 0);
+			parseInt(String(v).replace(Ext.data.Types.stripRe, ''), 10) : '';//(this.useNull ? null : 0) --> ''
 	},
 	sortType: Ext.data.SortTypes.none,
 	type: 'int'
@@ -469,12 +479,42 @@ Ext.data.Types.INT = {
 Ext.data.Types.FLOAT = {
 	convert: function(v){
 		return v !== undefined && v !== null && v !== '' ?
-			parseFloat(String(v).replace(Ext.data.Types.stripRe, ''), 10) : '';//(this.useNull ? null : 0);
+			parseFloat(String(v).replace(Ext.data.Types.stripRe, ''), 10) : '';//(this.useNull ? null : 0) --> ''
 	},
 	sortType: Ext.data.SortTypes.none,
 	type: 'float'
 };
+//在editgrid，如果可编辑字段为date类型，且值为空，则点击可编辑单元，光标离开时会给脏标记，
+//是因为startValue=null，而当前值为空，所以值改变了。特修改如下：如果值为空，则返回''
+Ext.data.Types.DATE = {
+	convert: function(v){
+		var df = this.dateFormat;
+		if(!v){
+			return '';//null -->''
+		}
+		if(Ext.isDate(v)){
+			return v;
+		}
+		if(df){
+			if(df == 'timestamp'){
+				return new Date(v*1000);
+			}
+			if(df == 'time'){
+				return new Date(parseInt(v, 10));
+			}
+			return Date.parseDate(v, df);
+		}
+		var parsed = Date.parse(v);
+		return parsed ? new Date(parsed) : '';//null -->''
+	},
+	sortType: Ext.data.SortTypes.asDate,
+	type: 'date'
+};
 
+/**
+ * ext-3.3.1
+ * 新增属性：添加24小时制时间格式校验
+ **/
 Ext.apply(Ext.form.VTypes, {
 	//24小时制时间格式校验
     time24: function(val, field) {
@@ -486,7 +526,7 @@ Ext.apply(Ext.form.VTypes, {
 });
 
 /**
- * 给Record对象添加一个取数字的方法。
+ * 新增方法：给Record对象添加一个取数字的方法。
  **/
 Ext.data.Record.prototype.getNum = function(name){
 	var value = this.data[name];
@@ -496,7 +536,8 @@ Ext.data.Record.prototype.getNum = function(name){
 };
 
 /**
- * 修改取字符长度的方法，如果是汉字则是两个字节。
+ * ext-3.3.1
+ * 修改方法：修改取字符长度的方法，如果是汉字则是两个字节。
  **/
 Ext.form.TextField.prototype.getErrors = function(value) {
 	var errors = Ext.form.TextField.superclass.getErrors.apply(this, arguments);
@@ -550,7 +591,8 @@ Ext.form.TextField.prototype.getErrors = function(value) {
 };
 
 /**
- * 增加一种不检查必填项的校验方法，暂时未使用
+ * ext-3.3.1
+ * 增加方法：增加一种不检查必填项的校验方法，暂时未使用
  **/
 Ext.form.BasicForm.prototype.isValidBlank = function(){
 	var valid = true;
