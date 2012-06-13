522505c9dcb45fae9671b1f9247d12db9bc7a2f5
myjxstar <myjxstar@gmail.com>
2012-06-13 04:43:32 +0000

    增加数据导入模板设计功能。

---
+++ jxstar-webapp/src/main/webapp/jxstar/studio/grid_imp_field.js
@@ -0,0 +1,107 @@
+﻿Jxstar.currentPage = function() {
+	var config = {param:{},initpage:function(page, define){},eventcfg:{}};
+
+	var impsrcData = Jxstar.findComboData('impsrc');
+	var datatypeData = Jxstar.findComboData('datatype');
+	var yesnoData = Jxstar.findComboData('yesno');
+
+	var cols = [
+	{col:{header:'序号', width:41, sortable:true, renderer:JxUtil.formatInt()}, field:{name:'imp_field__field_no',type:'int'}},
+	{col:{header:'字段名称', width:113, sortable:true}, field:{name:'imp_field__field_name',type:'string'}},
+	{col:{header:'字段标题', width:122, sortable:true}, field:{name:'imp_field__field_title',type:'string'}},
+	{col:{header:'来源', width:66, sortable:true, align:'center',
+		editable:false,
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: impsrcData
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false, allowBlank:false,
+			value: impsrcData[0][0]
+		}),
+		renderer:function(value){
+			for (var i = 0; i < impsrcData.length; i++) {
+				if (impsrcData[i][0] == value)
+					return impsrcData[i][1];
+			}
+		}}, field:{name:'imp_field__data_src',type:'string'}},
+	{col:{header:'位置', width:57, sortable:true}, field:{name:'imp_field__field_pos',type:'string'}},
+	{col:{header:'数据类型', width:65, sortable:true, align:'center',
+		editable:false,
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: datatypeData
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false, allowBlank:false,
+			value: datatypeData[0][0]
+		}),
+		renderer:function(value){
+			for (var i = 0; i < datatypeData.length; i++) {
+				if (datatypeData[i][0] == value)
+					return datatypeData[i][1];
+			}
+		}}, field:{name:'imp_field__data_type',type:'string'}},
+	{col:{header:'必填?', width:55, sortable:true, align:'center',
+		editable:false,
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: yesnoData
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false,
+			value: yesnoData[0][0]
+		}),
+		renderer:function(value){
+			for (var i = 0; i < yesnoData.length; i++) {
+				if (yesnoData[i][0] == value)
+					return yesnoData[i][1];
+			}
+		}}, field:{name:'imp_field__is_must',type:'string'}},
+	{col:{header:'主键', width:100, sortable:true, hidden:true}, field:{name:'imp_field__field_id',type:'string'}},
+	{col:{header:'定义ID', width:100, sortable:true, hidden:true}, field:{name:'imp_field__imp_id',type:'string'}}
+	];
+	
+	config.param = {
+		cols: cols,
+		sorts: null,
+		hasQuery: '1',
+		isedit: '1',
+		isshow: '0',
+		funid: 'imp_field'
+	};
+	
+	config.initpage = function(gridNode){
+		var grid = gridNode.page;
+		grid.on('rowclick', function(g, rowindex, e) {
+			var frm = Ext.get('frm_designer_import').dom;
+			if (frm == null) return;
+			var seldiv = frm.contentWindow.getSelectDiv();
+			if (seldiv != null) {
+				var record = g.getStore().getAt(rowindex);
+				
+				seldiv.oldRecord = seldiv.curRecord;
+				seldiv.curRecord = record;
+				seldiv.titleField = 'imp_field__field_title';
+				seldiv.positionField = 'imp_field__field_pos';
+			}
+		});
+	};
+		
+	return new Jxstar.GridNode(config);
+}
