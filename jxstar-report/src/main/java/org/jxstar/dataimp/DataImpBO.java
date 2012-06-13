002f2b67b8c6c1e15a8c9d0ae876f6f333389c95
myjxstar <myjxstar@gmail.com>
2012-06-13 03:53:39 +0000

    添加数据导入模板设置与数据导入功能实现。

---
+++ jxstar-report/src/main/java/org/jxstar/dataimp/DataImpBO.java
@@ -0,0 +1,305 @@
+/*
+ * Copyright(c) 2012 Donghong Inc.
+ */
+package org.jxstar.dataimp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.fileupload.FileItem;
+import org.jxstar.control.action.RequestContext;
+import org.jxstar.dao.DaoParam;
+import org.jxstar.dataimp.parse.DataParser;
+import org.jxstar.dataimp.parse.TxtDataParser;
+import org.jxstar.dataimp.parse.XlsDataParser;
+import org.jxstar.service.BusinessObject;
+import org.jxstar.util.MapUtil;
+import org.jxstar.util.factory.FactoryUtil;
+
+/**
+ * 公共数据导入处理类。
+ *
+ * @author TonyTan
+ * @version 1.0, 2012-6-11
+ */
+public class DataImpBO extends BusinessObject {
+	private static final long serialVersionUID = 1L;
+	//返回到环境变量中的导入数据的主键值队列
+	private static final String IMP_KEYIDS = "imp_keyids";
+
+	/**
+	 * 根据功能ID找到数据导入定义信息：构建表头数据定义对象、表格数据定义对象、关联关系定义对象、新增数据SQL对象；
+	 * 读取请求对象的中xls文件；
+	 * 解析xls文件中的数据；
+	 * 如果有表头字段：获取模板表头数据集；
+	 * 如果有表格字段：获取一行表格数据集；
+	 * 如果有关系字段：根据上面两个数据集中的值作为参数，解析关系SQL，获取关联数据集；
+	 * 判断必填，如果没有则不导入；
+	 * 调用新增SQL，新增一条记录；
+	 * 循环所有表格数据新增。
+	 * @param request
+	 * @return
+	 */
+	public String onDataImp(RequestContext request) {
+		//当前功能ID
+		String funId = request.getFunID();
+		//上传的导入数据文件
+		FileItem impFile = (FileItem) request.getRequestObject("import_file");
+		if (impFile == null) {
+			setMessage("没有找到上传的数据文件！");
+			return _returnFaild;
+		}
+		InputStream ins;
+		try {
+			ins = impFile.getInputStream();
+		} catch (IOException e) {
+			_log.showError(e);
+			setMessage("解析上传的数据文件出错：" + e.getMessage());
+			return _returnFaild;
+		}
+		
+		//取外键值
+		String fkValue = request.getRequestValue("fkValue");
+		//当前用户信息
+		Map<String,String> userInfo = request.getUserInfo();
+		
+		//解析数据，执行导入
+		List<String> lsImpKeys = dataImp(ins, funId, fkValue, userInfo);
+		if (lsImpKeys == null || lsImpKeys.isEmpty()) return _returnFaild;
+		
+		request.getRequestMap().put(IMP_KEYIDS, lsImpKeys);
+		return _returnSuccess;
+	}
+	
+	/**
+	 * 执行数据导入，方便测试。
+	 * @param ins
+	 * @param funId
+	 * @param fkValue
+	 * @param userInfo
+	 * @return
+	 */
+	public List<String> dataImp(InputStream ins, String funId, String fkValue, Map<String,String> userInfo) {
+		Map<String,String> mpImp = DataImpUtil.queryImp(funId);
+		if (mpImp.isEmpty()) {
+			setMessage("没有找到【{0}】功能的数据导入定义！", funId);
+			return null;
+		}
+		//模板文件类型：xls, csv
+		String tplType = mpImp.get("tpl_type");
+		//定义ID
+		String impId = mpImp.get("imp_id");
+		//第一行数据的位置
+		int firstRow = DataImpUtil.getFirstRow(impId); 
+		if (firstRow < 0) {
+			setMessage("第一行数据位置为【{0}】，不正确！", firstRow);
+			return null;
+		}
+		
+		//解析上传的文件
+		DataParser parser = null;
+		if (tplType.equals("xls")) {
+			parser = new XlsDataParser();
+		} else if (tplType.equals("txt") || tplType.equals("csv")) {
+			parser = new TxtDataParser();
+		} else {
+			setMessage("没有找到解析上传文件的对象！");
+			return null;
+		}
+		parser.init(ins, firstRow);
+		
+		//新增SQL对象
+		String insertSql = mpImp.get("insert_sql");
+		//解析目标SQL中的常量
+		insertSql = DataImpUtil.parseConstant(insertSql, userInfo);
+		//解析目标SQL中的外键值
+		insertSql = insertSql.replaceFirst(DataImpUtil.FKEYID_REGEX, DataImpUtil.addChar(fkValue));
+		
+		//解析数据，执行导入
+		List<String> lsImpKeys = importData(parser, impId, insertSql, funId);
+		if (lsImpKeys == null || lsImpKeys.isEmpty()) {
+			setMessage("执行数据导入操作失败！");
+			return null;
+		}
+		
+		return lsImpKeys;
+	}
+	
+	/**
+	 * 解析数据，根据定义，执行导入
+	 * @return
+	 */
+	private List<String> importData(DataParser parser, String impId, String insertSql, String funId) {
+		List<String> lsImpKeys = FactoryUtil.newList();
+		//表头定义
+		List<Map<String,String>> formField = DataImpUtil.queryDataField(impId, "2");
+		//解析表头中的数据
+		Map<String,String> formData = parseForm(parser, formField);
+		
+		//表格定义
+		List<Map<String,String>> lsGfield = DataImpUtil.queryDataField(impId, "1");
+		//解析表格中的数据
+		List<Map<String,String>> gridData = parseGrid(parser, lsGfield);
+		
+		//关系SQL定义
+		List<Map<String,String>> lsRelatSql = DataImpUtil.queryRelatSql(impId);
+		//是否有关系数据字段
+		boolean hasRelat = DataImpUtil.hasRelatField(impId);
+		
+		//新增SQL的参数
+		List<Map<String,String>> lsField = DataImpUtil.queryField(impId);
+		
+		//判断是否有主键标志与编码标志
+		boolean isNewKeyId = (insertSql.indexOf(DataImpUtil.NEW_KEYID) >= 0);
+		if (isNewKeyId) {
+			insertSql = insertSql.replaceFirst(DataImpUtil.NEW_KEYID_REGEX, "?");
+		}
+		boolean isNewCode = (insertSql.indexOf(DataImpUtil.NEW_CODE) >= 0);
+		if (isNewCode) {
+			insertSql = insertSql.replaceFirst(DataImpUtil.NEW_CODE_REGEX, "?");
+		}
+		_log.showDebug(".........insert sql:" + insertSql);
+		//构建参数对象
+		DaoParam param = _dao.createParam(insertSql);
+		param.setUseParse(true);
+		
+		//开始导入数据
+		for (Map<String,String> mpData : gridData) {
+			_log.showDebug("--------------------- start import new data ---------------------");
+			Map<String,String> relatData = null;
+			if (hasRelat) {//取得相关关系数据集
+				relatData = DataImpUtil.queryRelat(lsRelatSql, formData, mpData);
+			}
+			
+			//解析SQL中的主键、编码、常量
+			//String sql = ImpUtil.parseInsertSQL(funId, fkValue, baseSql, userInfo);
+			if (isNewKeyId) {
+				String keyId = DataImpUtil.getKeyValue(funId);
+				lsImpKeys.add(keyId);
+				param.addStringValue(keyId);
+			}
+			if (isNewCode) {
+				param.addStringValue(DataImpUtil.getCodeValue(funId));
+			}
+			
+			//取新增SQL的参数：如果是表头数据则从formData取值；如果是表格数据则从gridData取值；如果是关系数据则从relat取值
+			for (Map<String,String> mpField : lsField) {
+				String value = "";
+				String srcType = mpField.get("data_src");
+				String field_name = mpField.get("field_name");
+				String data_type = mpField.get("data_type");
+				String is_must = mpField.get("is_must");
+				
+				if (srcType.equals("1")) {
+					value = MapUtil.getValue(mpData, field_name);
+				} else if (srcType.equals("2")) {
+					value = MapUtil.getValue(formData, field_name);
+				} else if (srcType.equals("3")) {
+					if (relatData != null && !relatData.isEmpty()) {
+						value = MapUtil.getValue(relatData, field_name);
+					}
+				}
+				_log.showDebug("..........field_name={0}, data_type={1}, data_src={2}, value={3}", field_name, data_type, srcType, value);
+				if (is_must.equals("1") && value.length() == 0) {
+					_log.showDebug("..........field_name:{0} value is empty!!!", field_name);
+					return lsImpKeys;
+				}
+				
+				param.addValue(value);
+				param.addType(data_type);
+			}
+			
+			//执行新增操作
+			boolean bret = _dao.update(param);
+			param.clearParam();
+			
+			if (!bret) return lsImpKeys;
+		}
+		
+		return lsImpKeys;
+	}
+	
+	/**
+	 * 解析表格中的数据
+	 * @param parser
+	 * @param lsGfield
+	 * @return
+	 */
+	private List<Map<String,String>> parseGrid(DataParser parser, List<Map<String,String>> lsGfield) {
+		List<Map<String,String>> lsData = FactoryUtil.newList();
+		
+		int rowsNum = parser.getRowsNum();
+		int colsNum = parser.getColsNum();
+		_log.showDebug("..........rows num: {0}, cols num: {1}", rowsNum, colsNum);
+		if (rowsNum < 0 || colsNum < 0) {
+			_log.showWarn("parse grid data rowsnum is -1 or colsnum is -1!!");
+			return lsData;
+		}
+		
+		for (int i = 0; i < rowsNum; i++) {
+			_log.showDebug("..........parse row:" + i);
+			
+			Map<String,String> mpData = FactoryUtil.newMap();
+			//是否有效数据，如果有必填字段没有填写，则不添加
+			boolean isValid = true;
+			for (Map<String,String> mpField : lsGfield) {
+				String fieldPos = mpField.get("field_pos");
+				String fieldName = mpField.get("field_name");
+				String is_must = mpField.get("is_must");
+				
+				int[] pos = DataImpUtil.getPosition(fieldPos);
+				if (pos.length != 2) {
+					_log.showWarn("import data position [{0}] is error!!", fieldPos);
+					continue;
+				}
+				
+				String value = parser.getData(pos[0]+i, pos[1]);
+				if (is_must.equals("1") && value.length() == 0) {
+					_log.showDebug("..........parse row fieldname:[{0}] data is empty!!", fieldName);
+					isValid = false;
+					break;
+				}
+				
+				mpData.put(fieldName, value);
+			}
+			
+			if (isValid) {
+				_log.showDebug("..........parse row data:" + mpData);
+				lsData.add(mpData);
+			} else {
+				_log.showDebug("..........parse row data has not valid!!");
+			}
+		}
+		
+		return lsData;
+	}
+	
+	/**
+	 * 解析表头中的数据
+	 * @param parser
+	 * @param formField
+	 * @return
+	 */
+	private Map<String,String> parseForm(DataParser parser, List<Map<String,String>> formField) {
+		Map<String,String> mpData = FactoryUtil.newMap();
+		
+		for (Map<String,String> mpField : formField) {
+			String fieldPos = mpField.get("field_pos");
+			String fieldName = mpField.get("field_name");
+			
+			int[] pos = DataImpUtil.getPosition(fieldPos);
+			if (pos.length != 2) {
+				_log.showWarn("import data position [{0}] is error!!", fieldPos);
+				continue;
+			}
+			
+			String value = parser.getData(pos[0], pos[1]);
+			mpData.put(fieldName, value);
+		}
+		_log.showDebug("..........parse form data:" + mpData);
+		
+		return mpData;
+	}
+}
---
+++ jxstar-report/src/main/java/org/jxstar/dataimp/DataImpUtil.java
@@ -0,0 +1,288 @@
+/*
+ * Copyright(c) 2012 Donghong Inc.
+ */
+package org.jxstar.dataimp;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jxstar.dao.BaseDao;
+import org.jxstar.dao.DaoParam;
+import org.jxstar.service.control.ControlerUtil;
+import org.jxstar.service.define.FunctionDefine;
+import org.jxstar.service.define.FunctionDefineManger;
+import org.jxstar.service.util.TaskUtil;
+import org.jxstar.util.MapUtil;
+import org.jxstar.util.factory.FactoryUtil;
+import org.jxstar.util.key.CodeCreator;
+import org.jxstar.util.key.KeyCreator;
+import org.jxstar.util.log.Log;
+
+/**
+ * 数据导入工具方法。
+ *
+ * @author TonyTan
+ * @version 1.0, 2012-6-11
+ */
+public class DataImpUtil {
+	private static Log _log = Log.getInstance();
+	private static BaseDao _dao = BaseDao.getInstance();
+	//外键标志
+	public static final String FKEYID_REGEX = "\\{FKEYID\\}";
+	//新增主键标志
+	public static final String NEW_KEYID = "{NEWKEYID}";
+	public static final String NEW_KEYID_REGEX = "\\{NEWKEYID\\}";
+	//新增编号标志
+	public static final String NEW_CODE = "{NEWCODE}";
+	public static final String NEW_CODE_REGEX = "\\{NEWCODE\\}";
+	
+	/**
+	 * 取得关联关系数据
+	 * @param lsRelatSql
+	 * @param formData
+	 * @param gridData
+	 * @return
+	 */
+	public static Map<String,String> queryRelat(List<Map<String,String>> lsRelatSql,
+			Map<String,String> formData,
+			Map<String,String> gridData) {
+		//把表格与表头数据拼在一起作为关系SQL的where参数解析来源
+		Map<String,String> paramData = FactoryUtil.newMap();
+		if (formData != null && !formData.isEmpty()) paramData.putAll(formData);
+		if (gridData != null && !gridData.isEmpty()) paramData.putAll(gridData);
+		
+		Map<String,String> queryData = FactoryUtil.newMap();
+		for (Map<String,String> mpRelatSql : lsRelatSql) {
+			String sql = mpRelatSql.get("relat_sql");
+			if (sql.length() == 0) continue;
+			
+			sql = TaskUtil.parseAppField(sql, paramData, true);
+			_log.showDebug("..........relatsql:" + sql);
+			
+			DaoParam param = _dao.createParam(sql);
+			param.setUseParse(true);
+			Map<String,String> mp = _dao.queryMap(param);
+			
+			queryData.putAll(mp);
+		}
+		
+		return queryData;
+	}
+	
+	/**
+	 * 解析新增SQL中的ID与编码值：{NEWKEYID}{NEWCODE}
+	 * @param funId
+	 * @param insertSql
+	 * @return
+	 */
+	public static String parseInsertSQL(String funId, String fkValue, 
+			String insertSql, Map<String,String> userInfo) {
+		if (insertSql == null || insertSql.length() == 0) return "";
+		
+		//创建主键生成对象
+		KeyCreator keyCreator = KeyCreator.getInstance();
+		//创建编码生成对象
+		CodeCreator codeCreator = CodeCreator.getInstance();
+		//解析目标SQL中的常量
+		insertSql = parseConstant(insertSql, userInfo);
+		//解析目标SQL中的外键值
+		insertSql = insertSql.replaceFirst(FKEYID_REGEX, addChar(fkValue));
+		
+		//是否新增主键
+		boolean isNewKeyId = (insertSql.indexOf(NEW_KEYID) >= 0);
+		if (isNewKeyId) {
+			FunctionDefine funObject = FunctionDefineManger.getInstance().getDefine(funId);
+			String tableName = funObject.getElement("table_name");
+			String newKeyID = keyCreator.createKey(tableName);
+			insertSql = insertSql.replaceFirst(NEW_KEYID_REGEX, addChar(newKeyID));
+		}
+		
+		//是否新增编号
+		boolean isNewCode = (insertSql.indexOf(NEW_CODE) >= 0);
+		if (isNewCode) {
+			String newCode = codeCreator.createCode(funId);
+			insertSql = insertSql.replaceFirst(NEW_CODE_REGEX, addChar(newCode));
+		}
+		
+		return insertSql;
+	}
+	
+	/**
+	 * 取编码值
+	 * @param funId
+	 * @return
+	 */
+	public static String getCodeValue(String funId) {
+		CodeCreator codeCreator = CodeCreator.getInstance();
+		return codeCreator.createCode(funId);
+	}
+	
+	/**
+	 * 取主键值
+	 * @param funId
+	 * @return
+	 */
+	public static String getKeyValue(String funId) {
+		KeyCreator keyCreator = KeyCreator.getInstance();
+		FunctionDefine funObject = FunctionDefineManger.getInstance().getDefine(funId);
+		
+		String tableName = funObject.getElement("table_name");
+		return keyCreator.createKey(tableName);
+	}
+	
+	//取表格数据字段的位置信息，取行序号
+	public static int getFirstRow(String impId) {
+		String sql = "select field_pos from imp_field where data_src = '1' and imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(impId);
+		
+		Map<String,String> mp = _dao.queryMap(param);
+		if (mp.isEmpty()) return -1;
+		
+		String field_pos = mp.get("field_pos");
+		int[] pos = getPosition(field_pos);
+		if (pos.length != 2) {
+			return -1;
+		}
+		return pos[0];
+	}
+	
+	/**
+	 * 获取位置
+	 * @param position
+	 * @return
+	 */
+    public static int[] getPosition(String position) {
+        int [] ret = new int[0];
+        if (position == null || position.length() == 0) {
+            return ret;
+        }
+        String[] strRet = position.split(",");
+        if (strRet.length != 2) return ret;
+
+        ret = new int[2];
+        ret[0] = Integer.parseInt(strRet[0]);   //行
+        ret[1] = Integer.parseInt(strRet[1]);   //列
+
+        return ret;
+    }
+    
+	/**
+	 * 解析SQL中的常量值。
+	 * @param sql
+	 * @param userInfo
+	 * @return
+	 */
+	public static String parseConstant(String sql, 
+			Map<String,String> userInfo) {
+		String regex = "\\{[^}]+\\}";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(sql);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			String tag = m.group();
+			//取常量的值
+			String value = (String) ControlerUtil.getConstantParam(tag, userInfo);
+			//如果还含{，说明没有解析
+			if (value.indexOf("{") >= 0) {
+				m.appendReplacement(sb, value);
+			} else {
+				m.appendReplacement(sb, addChar(value));
+			}
+		}
+		m.appendTail(sb);
+		
+		return sb.toString();
+	}
+    
+	/**
+	 * 字符串两头加上'
+	 * @param str
+	 * @return
+	 */
+	public static String addChar(String str) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("'").append(str).append("'");
+		
+		return sb.toString();
+	}
+	
+	//取数据导入的SQL
+	public static Map<String,String> queryImp(String funId) {
+		String sql = "select tpl_type, insert_sql, imp_id from imp_list where fun_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(funId);
+		
+		return _dao.queryMap(param);
+	}
+	
+	//取数据导入的SQL
+	public static Map<String,String> queryImpById(String imp_id) {
+		String sql = "select tpl_type, insert_sql, tpl_file, imp_id from imp_list where imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(imp_id);
+		
+		return _dao.queryMap(param);
+	}
+	
+	//取表头字段定义，数据来源类型：1表格、2表头、3关系数据
+	public static List<Map<String,String>> queryDataField(String impId, String srcType) {
+		String sql = "select field_name, field_pos, is_must from imp_field where data_src = ? and imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(srcType);
+		param.addStringValue(impId);
+		
+		return _dao.query(param);
+	}
+	
+	//是否关系数据字段
+	public static boolean hasRelatField(String impId) {
+		String sql = "select count(*) as cnt from imp_field where data_src = '3' and imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(impId);
+		
+		Map<String,String> mpData = _dao.queryMap(param);
+		return MapUtil.hasRecord(mpData);
+	}
+	
+	//取关联关系SQL定义
+	public static List<Map<String,String>> queryRelatSql(String impId) {
+		String sql = "select relat_sql from imp_relat where imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(impId);
+		
+		return _dao.query(param);
+	}
+	
+	//取新增SQL的参数列表
+	public static List<Map<String,String>> queryField(String impId) {
+		String sql = "select field_name, data_type, data_src, is_must, field_title, field_pos from imp_field where imp_id = ? order by field_no";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(impId);
+		
+		return _dao.query(param);
+	}
+	
+	/**
+	 * 取模板中显示的字段
+	 * @param impId
+	 * @return
+	 */
+	public static List<Map<String,String>> queryTplField(String impId) {
+		String sql = "select field_title, field_pos from imp_field where data_src in ('1', '2') and imp_id = ?";
+		
+		DaoParam param = _dao.createParam(sql);
+		param.addStringValue(impId);
+		
+		return _dao.query(param);
+	}
+}
