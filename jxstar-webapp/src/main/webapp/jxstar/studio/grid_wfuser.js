5880c72803aa375425382c60ef144038873928ee
myjxstar <myjxstar@gmail.com>
2012-11-08 00:44:18 +0000

    条件长度调整到1000

--- jxstar-webapp/src/main/webapp/jxstar/studio/grid_wfuser.js
+++ jxstar-webapp/src/main/webapp/jxstar/studio/grid_wfuser.js
@@ -13,6 +13,20 @@
 	{col:{header:'部门编码', width:100, sortable:true, hidden:true}, field:{name:'sys_dept__dept_code',type:'string'}},
 	{col:{header:'部门名称', width:124, sortable:true}, field:{name:'sys_dept__dept_name',type:'string'}},
 	{col:{header:'是否定制类', width:70, sortable:true, hidden:true, defaultval:'0', align:'center',
+		editable:true, hcss:'color:#3039b4;',
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: yesnoData
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false,
+			value: yesnoData[0][0]
+		}),
 		renderer:function(value){
 			for (var i = 0; i < yesnoData.length; i++) {
 				if (yesnoData[i][0] == value)
@@ -21,7 +35,7 @@
 		}}, field:{name:'wf_user__use_class',type:'string'}},
 	{col:{header:'分配条件', width:256, sortable:true, editable:true, hcss:'color:#3039b4;',
 		editor:new Ext.form.TextField({
-			maxLength:200
+			maxLength:1000
 		})}, field:{name:'wf_user__cond_where',type:'string'}},
 	{col:{header:'升级用户', width:83, sortable:true, editable:true, hcss:'color:#3039b4;',
 		editor:new Ext.form.TriggerField({
@@ -29,7 +43,7 @@
 			editable:false,
 			triggerClass:'x-form-search-trigger', 
 			onTriggerClick: function() {
-				var selcfg = {pageType:'combogrid', nodeId:'sys_user', layoutPage:'/public/layout/layout_tree.js', sourceField:'sys_user.user_name;user_id', targetField:'wf_user.up_user;up_userid', whereSql:"sys_user.is_novalid = '0'", whereValue:'', whereType:'', isSame:'0', isShowData:'1', isMoreSelect:'0',isReadonly:'1',fieldName:'wf_user.up_user'};
+				var selcfg = {pageType:'combogrid', nodeId:'sys_user', layoutPage:'/public/layout/layout_tree.js', sourceField:'sys_user.user_name;user_id', targetField:'wf_user.up_user;up_userid', whereSql:"sys_user.is_novalid = '0'", whereValue:'', whereType:'', isSame:'0', isShowData:'1', isMoreSelect:'0',isReadonly:'1',queryField:'',likeType:'',fieldName:'wf_user.up_user'};
 				JxSelect.createSelectWin(selcfg, this, 'node_wf_user_editgrid');
 			}
 		})}, field:{name:'wf_user__up_user',type:'string'}},
@@ -51,6 +65,7 @@
 		isshow: '0',
 		funid: 'wf_user'
 	};
+	
 	
 	config.initpage = function(pageNode){		var event = pageNode.event;		event.on('beforeimport', function(){		var fkValue = pageNode.page.fkValue;		if (!fkValue) {			JxHint.alert(jx.util.selectno);			return false;		}		return true;	});
 };
