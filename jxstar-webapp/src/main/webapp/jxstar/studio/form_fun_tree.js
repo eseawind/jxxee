f9e0153680df1070bb92926c04e2b1b654c1a385
myjxstar <myjxstar@gmail.com>
2012-11-28 02:21:38 +0000

    调整了树形定义功能。

--- jxstar-webapp/src/main/webapp/jxstar/studio/form_fun_tree.js
+++ jxstar-webapp/src/main/webapp/jxstar/studio/form_fun_tree.js
@@ -2,6 +2,7 @@
 	var config = {param:{},initpage:function(page, define){},eventcfg:{}};
 	
 	var nodeclsData = Jxstar.findComboData('nodecls');
+	var charflagData = Jxstar.findComboData('charflag');
 	var treetypeData = Jxstar.findComboData('treetype');
 	var items = [{
 		width:'97%',
@@ -33,7 +34,7 @@
 					{xtype:'textfield', fieldLabel:'节点名字段', name:'fun_tree__node_name', allowBlank:false, labelStyle:'color:#0000FF;', labelSeparator:'*', anchor:'100%', maxLength:100},
 					{xtype:'textfield', fieldLabel:'级别字段', name:'fun_tree__node_level', allowBlank:false, labelStyle:'color:#0000FF;', labelSeparator:'*', anchor:'100%', maxLength:100},
 					{xtype:'textfield', fieldLabel:'目标过滤条件', name:'fun_tree__right_where', allowBlank:false, labelStyle:'color:#0000FF;', labelSeparator:'*', anchor:'100%', maxLength:100},
-					{xtype:'textfield', fieldLabel:'树形标题', name:'fun_tree__tree_title', anchor:'100%', maxLength:50},
+					{xtype:'textfield', fieldLabel:'树形标题', name:'fun_tree__tree_title', allowBlank:false, labelStyle:'color:#0000FF;', labelSeparator:'*', anchor:'100%', maxLength:50},
 					{xtype:'checkbox', fieldLabel:'是否含本级', name:'fun_tree__has_level', defaultval:'0', disabled:false, anchor:'100%'}
 				]
 			},{
@@ -48,8 +49,8 @@
 					{xtype:'textfield', fieldLabel:'ORDER子句', name:'fun_tree__self_order', anchor:'100%', maxLength:100},
 					{xtype:'textfield', fieldLabel:'数据源名', name:'fun_tree__db_name', anchor:'100%', maxLength:20},
 					{xtype:'textfield', fieldLabel:'节点附加值', name:'fun_tree__node_other', anchor:'100%', maxLength:100},
-					{xtype:'hidden', fieldLabel:'树形ID', name:'fun_tree__tree_id', anchor:'62%'},
-					{xtype:'hidden', fieldLabel:'所属功能ID', name:'fun_tree__fun_id', anchor:'62%'}
+					{xtype:'hidden', fieldLabel:'树形ID', name:'fun_tree__tree_id', anchor:'100%'},
+					{xtype:'hidden', fieldLabel:'所属功能ID', name:'fun_tree__fun_id', anchor:'100%'}
 				]
 			}
 			]
@@ -84,7 +85,18 @@
 						valueField: 'value',
 						displayField: 'text',
 						value: nodeclsData[0][0]},
-					{xtype:'textfield', fieldLabel:'树形组标志', name:'fun_tree__team_id', defaultval:'A', anchor:'100%', maxLength:25}
+					{xtype:'combo', fieldLabel:'树形组标志', name:'fun_tree__team_id', defaultval:'A',
+						anchor:'100%', editable:false,
+						store: new Ext.data.SimpleStore({
+							fields:['value','text'],
+							data: charflagData
+						}),
+						emptyText: jx.star.select,
+						mode: 'local',
+						triggerAction: 'all',
+						valueField: 'value',
+						displayField: 'text',
+						value: charflagData[0][0]}
 				]
 			},{
 				border:false,
