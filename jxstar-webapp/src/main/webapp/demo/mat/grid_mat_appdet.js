518534ab4d9cc27297e00493a3fe20d1a155e9a5
myjxstar <myjxstar@gmail.com>
2013-02-04 02:20:07 +0000

    整理了一下ext样式。

--- jxstar-webapp/src/main/webapp/demo/mat/grid_mat_appdet.js
+++ jxstar-webapp/src/main/webapp/demo/mat/grid_mat_appdet.js
@@ -2,10 +2,10 @@
 	var config = {param:{},initpage:function(page, define){},eventcfg:{}};
 
 	var cols = [
-	{col:{header:'*物资编码', width:100, sortable:true, editable:true, hcss:'color:#0000ff;',
+	{col:{header:'物资编码', width:100, sortable:true, editable:true, hcss:'color:#3039b4;',
 		editor:new Ext.form.ComboBox({
 			maxLength:1000, name:'mat_base__mat_code', 
-			editable:true, hcss:'color:#0000ff;', allowBlank:false,
+			editable:true, hcss:'color:#3039b4;',
 			triggerClass:'x-form-search-trigger', 
 			listeners:{afterrender: function(combo) {
 				JxSelect.initCombo('mat_appdet', combo, 'node_mat_appdet_editgrid');
