04cdd91d283eb8793356f5e6556f83e0d8d20819
myjxstar <myjxstar@gmail.com>
2013-03-29 12:50:58 +0000

    报表设计器增加状态、用途分类字段。

--- jxstar-webapp/src/main/webapp/jxstar/studio/grid_rpt_list.js
+++ jxstar-webapp/src/main/webapp/jxstar/studio/grid_rpt_list.js
@@ -1,24 +1,26 @@
 ﻿Jxstar.currentPage = function() {
 	var config = {param:{},initpage:function(page, define){},eventcfg:{}};
 
-	var repttypeData = Jxstar.findComboData('repttype');
-	var yesnoData = Jxstar.findComboData('yesno');
+	var Datarepttype = Jxstar.findComboData('repttype');
+	var Datayesno = Jxstar.findComboData('yesno');
+	var Dataregstatus = Jxstar.findComboData('regstatus');
+	var Datap_rpttype = Jxstar.findComboData('p_rpttype');
 
 	var cols = [
-	{col:{header:'序号', width:56, sortable:true, align:'right',
+	{col:{header:'序号', width:47, sortable:true, align:'right',
 		editable:true, hcss:'color:#3039b4;',
 		editor:new Ext.form.NumberField({
-			maxLength:12
+			decimalPrecision:0, maxLength:12
 		}),renderer:JxUtil.formatInt()}, field:{name:'rpt_list__report_index',type:'int'}},
-	{col:{header:'*报表名称', width:216, sortable:true, editable:true, hcss:'color:#0000ff;',
+	{col:{header:'*报表名称', width:211, sortable:true, editable:true, hcss:'color:#0000ff;',
 		editor:new Ext.form.TextField({
 			maxLength:50, allowBlank:false
 		})}, field:{name:'rpt_list__report_name',type:'string'}},
-	{col:{header:'*模板文件', width:235, sortable:true, editable:true, hcss:'color:#0000ff;',
+	{col:{header:'*模板文件', width:225, sortable:true, editable:true, hcss:'color:#0000ff;',
 		editor:new Ext.form.TextField({
 			maxLength:100, allowBlank:false
 		})}, field:{name:'rpt_list__report_file',type:'string'}},
-	{col:{header:'*所属功能', width:134, sortable:true, editable:true, hcss:'color:#0000ff;',
+	{col:{header:'*所属功能', width:122, sortable:true, editable:true, hcss:'color:#0000ff;',
 		editor:new Ext.form.TextField({
 			maxLength:25, allowBlank:false
 		})}, field:{name:'rpt_list__fun_id',type:'string'}},
@@ -31,7 +33,7 @@
 		editor:new Ext.form.ComboBox({
 			store: new Ext.data.SimpleStore({
 				fields:['value','text'],
-				data: repttypeData
+				data: Datarepttype
 			}),
 			emptyText: jx.star.select,
 			mode: 'local',
@@ -39,12 +41,12 @@
 			valueField: 'value',
 			displayField: 'text',
 			editable:false,
-			value: repttypeData[0][0]
+			value: Datarepttype[0][0]
 		}),
 		renderer:function(value){
-			for (var i = 0; i < repttypeData.length; i++) {
-				if (repttypeData[i][0] == value)
-					return repttypeData[i][1];
+			for (var i = 0; i < Datarepttype.length; i++) {
+				if (Datarepttype[i][0] == value)
+					return Datarepttype[i][1];
 			}
 		}}, field:{name:'rpt_list__report_type',type:'string'}},
 	{col:{header:'缺省?', width:48, sortable:true, defaultval:'1', align:'center',
@@ -52,7 +54,7 @@
 		editor:new Ext.form.ComboBox({
 			store: new Ext.data.SimpleStore({
 				fields:['value','text'],
-				data: yesnoData
+				data: Datayesno
 			}),
 			emptyText: jx.star.select,
 			mode: 'local',
@@ -60,14 +62,56 @@
 			valueField: 'value',
 			displayField: 'text',
 			editable:false,
-			value: yesnoData[0][0]
+			value: Datayesno[0][0]
 		}),
 		renderer:function(value){
-			for (var i = 0; i < yesnoData.length; i++) {
-				if (yesnoData[i][0] == value)
-					return yesnoData[i][1];
+			for (var i = 0; i < Datayesno.length; i++) {
+				if (Datayesno[i][0] == value)
+					return Datayesno[i][1];
 			}
 		}}, field:{name:'rpt_list__is_default',type:'string'}},
+	{col:{header:'状态', width:54, sortable:true, defaultval:'0', align:'center',
+		editable:true, hcss:'color:#3039b4;',
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: Dataregstatus
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false,
+			value: Dataregstatus[0][0]
+		}),
+		renderer:function(value){
+			for (var i = 0; i < Dataregstatus.length; i++) {
+				if (Dataregstatus[i][0] == value)
+					return Dataregstatus[i][1];
+			}
+		}}, field:{name:'rpt_list__status',type:'string'}},
+	{col:{header:'用途分类', width:72, sortable:true, align:'center',
+		editable:true, hcss:'color:#3039b4;',
+		editor:new Ext.form.ComboBox({
+			store: new Ext.data.SimpleStore({
+				fields:['value','text'],
+				data: Datap_rpttype
+			}),
+			emptyText: jx.star.select,
+			mode: 'local',
+			triggerAction: 'all',
+			valueField: 'value',
+			displayField: 'text',
+			editable:false,
+			value: Datap_rpttype[0][0]
+		}),
+		renderer:function(value){
+			for (var i = 0; i < Datap_rpttype.length; i++) {
+				if (Datap_rpttype[i][0] == value)
+					return Datap_rpttype[i][1];
+			}
+		}}, field:{name:'rpt_list__bus_type',type:'string'}},
 	{col:{header:'自定义报表类', width:136, sortable:true, editable:true, hcss:'color:#3039b4;',
 		editor:new Ext.form.TextField({
 			maxLength:100
@@ -76,7 +120,7 @@
 		editor:new Ext.form.TextField({
 			maxLength:100
 		})}, field:{name:'rpt_list__report_memo',type:'string'}},
-	{col:{header:'报表ID', width:100, sortable:true, hidden:true}, field:{name:'rpt_list__report_id',type:'string'}}
+	{col:{header:'报表ID', width:100, sortable:true, colindex:10000, hidden:true}, field:{name:'rpt_list__report_id',type:'string'}}
 	];
 	
 	config.param = {
@@ -89,6 +133,27 @@
 	};
 	
 	
+	var renderTask = function(value, metaData, record) {
+		var color = value == '0' ? 'green' : 'red'
+		var title = value || '';
+		var cbo = Jxstar.findComboData('regstatus');
+		for (var i = 0; i < cbo.length; i++) {
+			if (cbo[i][0] == value) {
+				title = cbo[i][1]; break;
+			}
+		}
+		
+		var html = '<span style="color:'+ color +'; font-weight:bold;">'+ title +'</span>';
+		return html;
+	};
+	
+	for (var i = 0; i < cols.length; i++) {
+		if (cols[i].field.name.indexOf('__status') > 0) {
+			cols[i].col.renderer = renderTask;
+			break;
+		}
+	}
+		
 	config.initpage = function(gridNode){
 		var event = gridNode.event;
 		event.on('beforecreate', function(event) {
